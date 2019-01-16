ec5611a467445f61bbac995ac9ee9e18f195ebde
Sireesha Bandela <siri@sireeshas-mbp-2.home>
2019-01-16 21:25:16 +0000

    Removed the titles and added the items in 1 container

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import breakingbadlogo from './images/BreakingBad_logo.jpeg'
 import deathnotelogo from './images/DeathNote_logo.jpg'
 import fearthewalkingdeadlogo from './images/FearTheWalkingDead.jpg'
 import gotlogo from './images/GOT_logo.jpeg'
-import thwirelogo from './images/TheWire_logo.jpg'
+import thewirelogo from './images/TheWire_logo.jpg'
 import './App.css';
 
 class App extends Component {
@@ -14,21 +14,27 @@ class App extends Component {
       <div className="App">
 
         <br /><br /><br />
-        <div className='container1'>
-          <div className='row1'>Black Mirror</div>
-          <img src={blackmirrorlogo} alt='Blackmirror logo' />
-          <div className='row1'>Breaking Bad</div>
-          <img src={breakingbadlogo} alt='BreakingBad logo' />
-          <div className='row1'>Death Note</div>
-          <img src={deathnotelogo} alt='DeathNote logo' />
+        <div className='container'>
+          <div className='item'>
+            <img src={blackmirrorlogo} alt="BlackMirrorLogo" />
+          </div>
+          <div className='item'>
+            <img src={breakingbadlogo} alt='BreakingBadLogo' />
+          </div>
+          <div className='item'>
+            <img src={deathnotelogo} alt='DeathNoteLogo' />
+          </div>
         </div>
-        <div className='container2'>
-          <div className='row2'>Game of Thrones</div>
-          <img src={gotlogo} alt='GameofThrones logo' />
-          <div className='row2'>The Walking Dead</div>
-          <img src={fearthewalkingdeadlogo} alt='FearTheWalkingDead logo' />
-          <div className='row2'>The Wire</div>
-          <img src={thwirelogo} alt='TheWire logo' />
+        <div className='container'>
+          <div className='item'>
+            <img src={gotlogo} alt='GotLogo' />
+          </div>
+          <div className='item'>
+            <img src={fearthewalkingdeadlogo} alt='FearTheWalkingDeadLogo' />
+          </div>
+          <div className='item'>
+            <img src={thewirelogo} alt='TheWireLogo' />
+          </div>
         </div>
       </div>
     );
