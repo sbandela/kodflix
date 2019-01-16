116ad07743300a423b6f377d0d2e2f39c98b718e
Sireesha Bandela <siri@Sireeshas-MacBook-Pro-2.local>
2019-01-16 08:26:03 +0000

    Add your favourite movie cover #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import blackmirrorlogo from './images/BlackMirror_logo.jpg'
+import breakingbadlogo from './images/BreakingBad_logo.jpeg'
+import deathnotelogo from './images/DeathNote_logo.jpg'
+import fearthewalkingdeadlogo from './images/FearTheWalkingDead.jpg'
+import gotlogo from './images/GOT_logo.jpeg'
+import thwirelogo from './images/TheWire_logo.jpg'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to react tutorials and Javascript tutorial.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <br /><br /><br />
+        <div className='container1'>
+          <div className='row1'>Black Mirror</div>
+          <img src={blackmirrorlogo} alt='Blackmirror logo' />
+          <div className='row1'>Breaking Bad</div>
+          <img src={breakingbadlogo} alt='BreakingBad logo' />
+          <div className='row1'>Death Note</div>
+          <img src={deathnotelogo} alt='DeathNote logo' />
+        </div>
+        <div className='container2'>
+          <div className='row2'>Game of Thrones</div>
+          <img src={gotlogo} alt='GameofThrones logo' />
+          <div className='row2'>The Walking Dead</div>
+          <img src={fearthewalkingdeadlogo} alt='FearTheWalkingDead logo' />
+          <div className='row2'>The Wire</div>
+          <img src={thwirelogo} alt='TheWire logo' />
+        </div>
       </div>
     );
   }
