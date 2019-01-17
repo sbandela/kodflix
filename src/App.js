3b419fc957cd17d3b8c79c2209207165ad42ce19
Sireesha Bandela <siri@Sireeshas-MacBook-Pro-2.local>
2019-01-17 17:22:28 +0000

    Issue#3 Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -17,23 +17,41 @@ class App extends Component {
         <div className='container'>
           <div className='item'>
             <img src={blackmirrorlogo} alt="BlackMirrorLogo" />
+            <div className='overlay'>
+              <h1>Black Mirror</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={breakingbadlogo} alt='BreakingBadLogo' />
+            <div className='overlay'>
+              <h1>Breaking Bad</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={deathnotelogo} alt='DeathNoteLogo' />
+            <div className='overlay'>
+              <h1>Death Note</h1>
+            </div>
           </div>
         </div>
         <div className='container'>
           <div className='item'>
             <img src={gotlogo} alt='GotLogo' />
+            <div className='overlay'>
+              <h1>Game of Thrones</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={fearthewalkingdeadlogo} alt='FearTheWalkingDeadLogo' />
+            <div className='overlay'>
+              <h1>Fear The Walking Dead</h1>
+            </div>
           </div>
           <div className='item'>
             <img src={thewirelogo} alt='TheWireLogo' />
+            <div className='overlay'>
+              <h1>The Wire</h1>
+            </div>
           </div>
         </div>
       </div>
